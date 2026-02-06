38e80ea8ccf2774333dcdce37d71c0f5fdb55c66
Daniel. <santiagomontoyamurcia@gmail.com>
2026-02-06 15:10:17 -0500

    Main.c

---
+++ Corte1/C/Practica1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Biblioteca estándar para entrada/salida
+
+int main() {
+    // printf imprime texto en pantalla
+    printf("Hola, Mundo!\n");
+    
+    return 0; // Indica que el programa finalizó correctamente
+}
