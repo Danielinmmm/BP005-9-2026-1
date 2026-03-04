fba57d44b5b8295ff1fc892f168217a6999c0999
Daniel. <santiagomontoyamurcia@gmail.com>
2026-03-04 13:20:29 -0500

    Create thanxy.c

---
+++ Corte1/C/Practica2/thanxy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int x, y;
+    printf("Ingrese el primer número x\n");
+    scanf("%d", &x);
+    printf("Ingrese el segundo número y\n");
+    scanf("%d", %y);
+
+  if (x > y) {
+  printf("x es más grande que y");
+  else {
+  printf("x no es más grande que y");
+  }
+    return 0;
+  }
