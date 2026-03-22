18a377731d2fe0d716dae0be7ee7259016b0296a
Daniel. <santiagomontoyamurcia@gmail.com>
2026-03-22 12:34:35 -0500

    Implement double addition in codedouble.c
    Fix typos in printf function calls and add double input handling.

---
+++ Corte1/C/Practica1/codedouble.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  double num1, num2, suma;
+
+prinft("Ingrese el primer número (double): ");
+scanf("%lf", &num1);
+
+printf("Ingrese el segundo numero (double): ");
+scanf("%lf", &num2);
+
+
+suma = num1 + num2;
+
+prinft("La suma de %.2f y %.2f es: %2f\n", num1, num2, suma);
+
+
+return 0;
+
+}
