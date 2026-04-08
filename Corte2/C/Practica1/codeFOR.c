673f50206e795cbeeddbe426d559920b2aff4be7
Daniel. <santiagomontoyamurcia@gmail.com>
2026-04-08 13:28:55 -0500

    Create codeFOR.c

---
+++ Corte2/C/Practica1/codeFOR.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int 1; //Variable de control
+
+  for (1 = 1; 1 <=5; 1++) { //Inicio, condicion y actualizacion
+    printf("%d\n", 1); //Muestra el valor actual
+}
+  return 0;
+}
+
+  
