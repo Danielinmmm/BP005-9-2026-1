5d9ae4fd86ac48593ce13c339019de58f5ce4784
Daniel. <santiagomontoyamurcia@gmail.com>
2026-04-08 13:19:53 -0500

    Create Docode.c

---
+++ Corte2/C/Practica1/Docode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int 1 = 1; //Variable de control
+  do {
+    printf("%d\n", 1);  //Muestra el valor actual
+    1++; //Actualiza la variable
+  }
+    while (1 <= 5); //La condicion se verifica al final
+  
+return 0;
+}
