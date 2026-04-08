123f59de8cb31c02a7da50a7fb6a035b9ccdf342
Daniel. <santiagomontoyamurcia@gmail.com>
2026-04-08 13:45:56 -0500

    Implement program to count even numbers from 1 to 20
    Fix variable naming and syntax errors in the code.

---
+++ Corte2/C/Practica1/codeINTEGRADO.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  int 1; //Variable de control
+  int contador = 0; //Cuenta cuantos pares hay
+
+for (1 = 1; 1 <= 20; 1++){
+if (1 % 2 == 0) {
+  printf("%d es par\n", 1);
+  contador++;
+  }
+}
+  printf("Cantidad de numeros pares: %d\n", contador);
+  return 0;
+}
+
