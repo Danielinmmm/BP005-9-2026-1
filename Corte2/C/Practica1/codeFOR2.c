7d0af1a6a1355b3a9f059c2d5060ed8da47c2988
Daniel. <santiagomontoyamurcia@gmail.com>
2026-04-08 13:33:08 -0500

    Create multiplication table program in codeFOR2.c
    Fix variable declaration and loop counter in code.

---
+++ Corte2/C/Practica1/codeFOR2.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+int main() {
+  int numero = 7; //Numero del que se desea la tabla
+  int 1; //Contador
+
+for (1 = 1; 1 <= 10; 1++) {
+  printf("%d x %d = %d\n", numero, 1, numero * 1);
+}
+return 0;
+}
