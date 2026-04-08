6c9af0da07908b3d2894410e1f38f0bab242ed54
Daniel. <santiagomontoyamurcia@gmail.com>
2026-04-08 13:13:37 -0500

    Create codeWhile.c

---
+++ Corte2/C/Practica1/codeWhile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int 1 = 1; //Variable de control.
+  while (1 <= 5) { //Mientras 1 sea menor o igual que 5.
+  printf("%d\n", 1); //Actualiza la variable.
+  1++;
+  }
+
+  return 0;
+}
