1c02549d270968f9507a508be97a21d92d4de5f1
Daniel. <santiagomontoyamurcia@gmail.com>
2026-04-08 13:35:12 -0500

    Create aumFOR.c

---
+++ Corte2/C/Practica1/aumFOR.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int 1; //Variable de control
+    int suma = 0; //Acumulador
+
+for (1 = 1; 1 <= 10; 1++) {
+  suma = suma  + 1; //Acumula el valor actual de 1
+}
+  printf("La suma es: %d\n", suma);
+return 0;
+}
