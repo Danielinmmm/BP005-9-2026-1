14b1fc683713d961676db46684d99b1219c0d0b7
Daniel. <santiagomontoyamurcia@gmail.com>
2026-05-26 21:41:41 -0500

    Create Cuadrado6x6@.cpp

---
+++ Corte2/FigurasLoops/C++/Cuadrado6x6@.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int fila = 1; fila <= 6; fila++) {
+        for (int columna = 1; columna <= 6; columna++) {
+            cout << "@";
+        }
+        cout << endl;
+    }
+    return 0;
+}
