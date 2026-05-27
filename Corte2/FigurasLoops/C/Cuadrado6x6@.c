6e951b4214358f15534bc50c42ea4037ac109e14
Daniel. <santiagomontoyamurcia@gmail.com>
2026-05-26 20:46:48 -0500

    Add program to print a 6x6 square of '@'

---
+++ Corte2/FigurasLoops/C/Cuadrado6x6@.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int fila = 1;
+    int columna;
+
+    while (fila <= 6) {
+        columna = 1;
+
+        while (columna <= 6) {
+            printf("@");
+            columna++;
+        }
+
+        printf("\n");
+        fila++;
+    }
+
+    return 0;
+}
