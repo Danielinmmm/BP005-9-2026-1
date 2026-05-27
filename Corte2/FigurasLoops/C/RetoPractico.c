5732f62745cfa4869f2345539b95bb00aa21a590
Daniel. <santiagomontoyamurcia@gmail.com>
2026-05-26 21:48:43 -0500

    Create RetoPractico.c

---
+++ Corte2/FigurasLoops/C/RetoPractico.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+int n;
+int fila, espacios, simbolos;
+
+    printf("Ingrese el tamaño del rombo: ");
+    scanf("%d", &n);
+
+    for (fila = 1; fila <= n; fila++) {
+
+        for (espacios = 1; espacios <= n - fila; espacios++) {
+            printf(" ");
+        }
+        for (simbolos = 1; simbolos <= 2 * fila - 1; simbolos++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for (fila = n - 1; fila >= 1; fila--) {
+
+        for (espacios = 1; espacios <= n - fila; espacios++) {
+            printf(" ");
+        }
+        for (simbolos = 1; simbolos <= 2 * fila - 1; simbolos++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
