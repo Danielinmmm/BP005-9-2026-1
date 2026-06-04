1b39ef5a941e0ccb4489f69401fcbb60ce054e5f
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-03 23:25:09 -0500

    Add sqrt, pow, fabs, round, floor, and ceil examples

---
+++ Corte2/PrimerDocumentoFINAL/sqrt-pow-fabs-round-floor-ceil.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    printf("sqrt(25) = %.2f\n", sqrt(25));
+    printf("pow(2,3) = %.2f\n", pow(2,3));
+    printf("fabs(-7.5) = %.2f\n", fabs(-7.5));
+    printf("round(4.6) = %.2f\n", round(4.6));
+    printf("floor(4.9) = %.2f\n", floor(4.9));
+    printf("ceil(4.1) = %.2f\n", ceil(4.1));
+
+    return 0;
+}
