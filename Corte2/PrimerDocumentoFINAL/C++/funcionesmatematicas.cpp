4015cee01e054eacce09899015dbc9a869cc2148
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 19:54:47 -0500

    Add mathematical functions demonstration in C++

---
+++ Corte2/PrimerDocumentoFINAL/C++/funcionesmatematicas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    cout << "sqrt(25) = " << sqrt(25) << endl;
+    cout << "pow(2,3) = " << pow(2,3) << endl;
+    cout << "fabs(-7.5) = " << fabs(-7.5) << endl;
+    cout << "round(4.6) = " << round(4.6) << endl;
+    cout << "floor(4.9) = " << floor(4.9) << endl;
+    cout << "ceil(4.1) = " << ceil(4.1) << endl;
+
+    return 0;
+}
