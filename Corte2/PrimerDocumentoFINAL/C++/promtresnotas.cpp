535554dd16043b2c132b68eb195e90f50b55b20f
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 19:50:47 -0500

    Create promtresnotas.cpp

---
+++ Corte2/PrimerDocumentoFINAL/C++/promtresnotas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float n1, n2, n3;
+
+    cin >> n1 >> n2 >> n3;
+
+    float promedio = (n1 + n2 + n3) / 3;
+
+    cout << "Promedio = " << promedio;
+
+    return 0;
+}
