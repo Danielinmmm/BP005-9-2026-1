52b5c57c2387aa60c9fc20957cfe40c4598b31bf
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 21:18:59 -0500

    Create punto.c

---
+++ Corte2/TercerDocumentoFINAL/punto.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include "punto.h"
+
+void inicializarPunto(Punto *p,float x,float y){
+
+    p->x = x;
+    p->y = y;
+}
+
+void moverPunto(Punto *p,float dx,float dy){
+
+    p->x += dx;
+    p->y += dy;
+}
+
+float calcularDistanciaAlOrigen(Punto p){
+
+    return sqrtf(p.x*p.x + p.y*p.y);
+}
