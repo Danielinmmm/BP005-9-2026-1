c0ee063e33f9d94bb330a248e2f86d8e8c97efbd
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 21:18:51 -0500

    Add punto.h header file for Punto structure and functions

---
+++ Corte2/TercerDocumentoFINAL/punto.h
@@ -0,0 +1,13 @@
+#ifndef PUNTO_H
+#define PUNTO_H
+
+typedef struct{
+    float x;
+    float y;
+} Punto;
+
+void inicializarPunto(Punto *p,float x,float y);
+void moverPunto(Punto *p,float dx,float dy);
+float calcularDistanciaAlOrigen(Punto p);
+
+#endif
