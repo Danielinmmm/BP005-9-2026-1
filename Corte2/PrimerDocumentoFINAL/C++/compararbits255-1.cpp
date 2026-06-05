8def26fe2f825f0a127ca38522764a1ee723a723
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 19:53:41 -0500

    Add compararbits255-1.cpp to demonstrate bit comparison

---
+++ Corte2/PrimerDocumentoFINAL/C++/compararbits255-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main() {
+
+    uint8_t x = 255;
+    int8_t y = -1;
+
+    cout << "x = " << (int)x << endl;
+    cout << "y = " << (int)y << endl;
+
+    return 0;
+}
