78e7708dacd5b367f2c85312c9e53f77ad985888
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 20:30:15 -0500

    Create conpunteros.c

---
+++ Corte2/SegundodocumentoFINAL/C/conpunteros.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct {
+    float velocidad;
+    float voltaje;
+} Motor;
+
+void cambiarVelocidad(Motor *m) {
+
+    m->velocidad = 5.0;
+}
+
+int main() {
+
+    Motor motor1 = {2.0, 6.0};
+
+    cambiarVelocidad(&motor1);
+
+    printf("%.2f\n", motor1.velocidad);
+
+    return 0;
+}
