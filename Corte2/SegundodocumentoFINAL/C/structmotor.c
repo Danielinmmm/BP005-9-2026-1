8285bc5d6da88a244a1fd3d7c86f59c37c2b1a6c
Daniel. <santiagomontoyamurcia@gmail.com>
2026-06-04 20:29:25 -0500

    Create structmotor.c

---
+++ Corte2/SegundodocumentoFINAL/C/structmotor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Motor {
+    float velocidad;
+    float voltaje;
+    int sentido;
+};
+
+int main() {
+
+    struct Motor m1 = {2.5, 8.0, 1};
+
+    printf("Velocidad: %.2f\n", m1.velocidad);
+    printf("Voltaje: %.2f\n", m1.voltaje);
+    printf("Sentido: %d\n", m1.sentido);
+
+    return 0;
+}
